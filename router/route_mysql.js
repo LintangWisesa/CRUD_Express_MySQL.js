c87c49ed2d991985218d11452d543a32786613b4
lintangwisesa <lintangwisesa@ymail.com>
2019-01-22 11:30:30 +0700

    mysql.js

--- router/route_mysql.js
+++ router/route_mysql.js
@@ -1,7 +1,45 @@
 var router = require('express').Router()
+var mysql = require('mysql')
+var bodyParser = require('body-parser')
+router.use(bodyParser.json())
 
-router.get('/data', (req, res) => {
-    res.send('<h1>Ini data</h1>')
+var db = mysql.createConnection({
+    host: 'localhost',
+    user: 'lintang',
+    password: '12345',
+    database: 'tokoku'
 })
+
+db.connect(()=>{
+    console.log('Database terhubung!')
+})
+
+// GET all data
+router.get('/data', (req, res) => {
+    var dbStat = 'select * from products'
+    db.query(dbStat, (error, output) => {
+        if(error){
+            console.log(error)
+        } else {
+            console.log(output)
+            res.send(output)
+        }
+    })
+})
+
+// GET data by id
+router.get('/data/:id', (req, res) => {
+    var dbStat = 'select * from products where id = ?'
+    db.query(dbStat, req.params.id, (error, output) => {
+        if(error){
+            console.log(error)
+        } else {
+            console.log(output)
+            res.send(output)
+        }
+    })
+})
+
+
 
 module.exports = router
